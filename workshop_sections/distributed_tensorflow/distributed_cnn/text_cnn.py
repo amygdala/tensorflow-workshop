255188b06cf4c7c2a46cb43e98a3d69a81f64e56
elibixby <elibixby@google.com>
2016-05-25 14:25:33 -0700

    Typo

--- workshop_sections/distributed_tensorflow/distributed_cnn/text_cnn.py
+++ workshop_sections/distributed_tensorflow/distributed_cnn/text_cnn.py
@@ -44,7 +44,7 @@ class DistributedTextCNN(object):
             self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
             input_x_minibatches = tf.split(0, len(workers), self.input_x)
             self.input_y =  tf.placeholder(tf.float32, [None, num_classes], name="input_y")
-            input_y_minibatches = tf.split(0, len(workers))
+            input_y_minibatches = tf.split(0, len(workers), self.input_y)
             self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
         # Keeping track of l2 regularization loss (optional)
