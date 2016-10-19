8b41a1541357ec55c60ee063956a0a925aa0efa3
Eli Bixby <elibixby@google.com>
2016-10-19 03:45:31 +0100

    Move to string inputs for word2vec

--- workshop_sections/word2vec/README.md
+++ workshop_sections/word2vec/README.md
@@ -42,20 +42,20 @@ or copy paste into Jupyter cells.
 ```
 import word2vec
 with open('text8', 'r') as f:
-    index, word_indices = word2vec.build_string_index(f.read())
+    index, counts, word_array = word2vec.build_string_index(f.read())
 ```
 (this may take ~1 minute)
 
 `word2vec.build_string_index` reads in a string tokenizes it using `nltk.word_tokenize` and builds
 
- * `index`: A `np.ndarray` that lists all unique words from most common to least common (+ `'UNK'` a token used to denote words which do not appear often enough in our fixed size vocabulary)`
+ * `index`: A `np.ndarray` that lists all unique words from most common to least common (+ `'UNK'` a token used to denote words which do not appear often enough in our fixed size vocabulary)
 
- * `word_indices`: A `np.ndarray` that replaces all the words in the string with their index in `index`. 
+ * `counts`: A `np.ndarray` with the counts of the corresponding words in `index`
 
-So `index[word_indices]` reconstructs the list, with less common words replaced by `UNK`.
+ * `word_array`: A `np.ndarray` of words 
 
 ```
-targets, contexts = word2vec.generate_batches(word_indices)
+targets, contexts = word2vec.generate_batches(word_array)
 ```
 (this may take 5-10 minutes)
 
@@ -76,13 +76,13 @@ import time
 workshop_root = os.path.dirname(os.path.dirname(os.getcwd()))
 output_dir = os.path.join(workshop_root, 'workshop-data', 'output', str(int(time.time())))
 tf.logging.set_verbosity(tf.logging.INFO)
-word2vec_model = tf.contrib.learn.Estimator(model_fn=word2vec.make_model_fn(), model_dir=output_dir)
+word2vec_model = tf.contrib.learn.Estimator(model_fn=word2vec.make_model_fn(index, counts), model_dir=output_dir)
 ```
 
 To train with the in memory numpy arrays, run:
 
 ```
-word2vec_model.fit(x=targets, y=contexts, batch_size=256, max_steps=50000)
+word2vec_model.fit(x=targets, y=contexts, batch_size=256, max_steps=int(len(targets)//256))
 ```
 
 To train with the pre-preprocessed TFRecords, run:
@@ -93,7 +93,7 @@ word2vec_model.fit(input_fn=word2vec.input_from_files(word2vec.GCS_SKIPGRAMS, 25
 In another shell (or outside your docker container), run
 
 ```sh
-tensorboard --logdir wordshop
+tensorboard --logdir workshop-data/output
 ```
 
 to watch the training outputs.
@@ -102,26 +102,18 @@ to watch the training outputs.
 
 This word2vec model predicts similarities between words using it's trained embeddings.
 
-When given a vector of word_indices, the word2vec model outputs a prediction dictionary with two values: `'predictions'` are the indices of the words most similar to the provided words, while `'values'` is a measure of their similarity to the provided words. You can use `index` to convert these word indices back into strings.
-
-NOTE: if you used TFRecords for training and as such don't have an `index` you can download it [here](https://storage.googleapis.com/oscon-tf-workshop-materials/stringindex.npy), and load it from this file using:
-
-```
-import numpy as np
-index = np.load('stringindex.npy')
-```
-
+When given a vector of words, the word2vec model outputs a prediction dictionary with two values: `'predictions'` are the words most similar to the provided words, while `'values'` is a measure of their similarity to the provided words. You can use `index` to convert these word indices back into strings.
 To predict, simply input a pandas dataframe or numpy array into `word2vec_model.predict`. The `num_sim` parameter in the original `make_model_fn` call controls the number of similar words we predict for each word in the input vector. You can make a new estimator object without retraining as long as you specify the same `model_dir` as your trained model. Trained parameters will be loaded from the most recent checkpoint!
 
 
 For example, predicting the 5 most common known words:
 
 ```
-index[word2vec_model.predict(x=np.array(range(1, 6)))['predictions']]
+word2vec_model.predict(x=index[5:])
 ```
 
 Or predicting a specific word:
 
 ```
-index[word2vec_model.predict(x=np.where(index == 'cat')[0])['predictions']]
+word2vec_model.predict(x=np.array(['government']))
 ```
--- workshop_sections/word2vec/word2vec.py
+++ workshop_sections/word2vec/word2vec.py
@@ -32,15 +32,28 @@ GCS_SKIPGRAMS = [
 ]
 
 
-def make_model_fn(num_partitions=1,
+def make_model_fn(index,
+                  vocab_counts,
+                  num_partitions=1,
                   embedding_size=128,
                   vocab_size=2 ** 15,
                   num_sim=8,
                   num_sampled=64,
                   learning_rate=0.1):
   def _make_model(target_words, context_words, mode):
-    with tf.device(tf.train.replica_device_setter()):
+    index_tensor = tf.constant(index)
+    reverse_index = tf.contrib.lookup.HashTable(
+        tf.contrib.lookup.KeyValueTensorInitializer(
+            index_tensor, tf.constant(range(vocab_size - 1), dtype=tf.int64)
+        ),
+        vocab_size - 1
+    )
 
+    # tf.contrib.learn.Estimator.fit adds an addition dimension to input
+    target_words_squeezed = tf.squeeze(target_words, squeeze_dims=[1])
+    target_indices = reverse_index.lookup(target_words_squeezed)
+
+    with tf.device(tf.train.replica_device_setter()):
       with tf.variable_scope('nce',
                              partitioner=tf.fixed_size_partitioner(
                                  num_partitions)):
@@ -51,34 +64,45 @@ def make_model_fn(num_partitions=1,
             dtype=tf.float32,
             initializer=tf.random_uniform_initializer(-1.0, 1.0)
         )
-        nce_weights = tf.get_variable(
-            'nce_weights',
-            shape=[vocab_size, embedding_size],
-            dtype=tf.float32,
-            initializer=tf.truncated_normal_initializer(
-                stddev=1.0 / math.sqrt(embedding_size)
-            )
-        )
-        nce_biases = tf.get_variable(
-            'nce_biases',
-            initializer=tf.zeros_initializer([vocab_size]),
-            dtype=tf.float32
-        )
+        if mode in [ModeKeys.TRAIN, ModeKeys.EVAL]:
+          nce_weights = tf.get_variable(
+              'nce_weights',
+              shape=[vocab_size, embedding_size],
+              dtype=tf.float32,
+              initializer=tf.truncated_normal_initializer(
+                  stddev=1.0 / math.sqrt(embedding_size)
+              )
+          )
+          nce_biases = tf.get_variable(
+              'nce_biases',
+              initializer=tf.zeros_initializer([vocab_size]),
+              dtype=tf.float32
+          )
 
-      predictions, loss, train_op = ({}, None, None)
-      # tf.contrib.learn.Estimator.fit adds an addition dimension to input
-      target_words_squeezed = tf.squeeze(target_words, squeeze_dims=[1])
+      prediction_dict, loss, train_op = ({}, None, None)
 
       if mode in [ModeKeys.TRAIN, ModeKeys.EVAL]:
-        embedded = tf.nn.embedding_lookup(embeddings, target_words_squeezed)
-        target_words = tf.expand_dims(context_words, 1)
+        context_indices = tf.expand_dims(
+            reverse_index.lookup(context_words), 1)
+        embedded = tf.nn.embedding_lookup(embeddings, target_indices)
+
+        sampled_words = tf.nn.fixed_unigram_candidate_sampler(
+            true_classes=context_indices,
+            num_true=1,
+            num_sampled=num_sampled,
+            unique=True,
+            range_max=vocab_size,
+            distortion=0.75,
+            unigrams=vocab_counts + [1]
+        )
         loss = tf.reduce_mean(tf.nn.nce_loss(
             nce_weights,
             nce_biases,
             embedded,
-            target_words,
+            context_indices,
             num_sampled,
-            vocab_size
+            vocab_size,
+            sampled_values=sampled_words
         ))
         tf.scalar_summary('loss', loss)
 
@@ -92,13 +116,15 @@ def make_model_fn(num_partitions=1,
         norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
         normalized_embeddings = embeddings / norm
         valid_embeddings = tf.nn.embedding_lookup(
-            normalized_embeddings, target_words_squeezed)
+            normalized_embeddings, target_indices)
         similarity = tf.matmul(
             valid_embeddings, normalized_embeddings, transpose_b=True)
-        predictions['values'], predictions['predictions'] = tf.nn.top_k(
+        prediction_dict['values'], predictions = tf.nn.top_k(
             similarity, sorted=True, k=num_sim)
+        index_tensor = tf.concat(0, [index_tensor, tf.constant(['UNK'])])
+        prediction_dict['predictions'] = tf.gather(index_tensor, predictions)
 
-      return predictions, loss, train_op
+      return prediction_dict, loss, train_op
   return _make_model
 
 
@@ -117,11 +143,11 @@ def to_skipgrams(window, skip_window, num_skips):
   return np.concatenate((targets, contexts))
 
 
-def generate_batches(word_indices, num_skips=2, skip_window=1):
+def generate_batches(word_array, num_skips=2, skip_window=1):
   assert num_skips <= 2 * skip_window
   span = 2 * skip_window + 1
 
-  span_windows = _rolling_window(word_indices, span)
+  span_windows = _rolling_window(word_array, span)
   batches = np.apply_along_axis(
     to_skipgrams, 1, span_windows, skip_window, num_skips)
   # Separate targets and contexts
@@ -132,29 +158,16 @@ def generate_batches(word_indices, num_skips=2, skip_window=1):
   batches_squashed = np.reshape(batches_gathered, (2, -1))
   return batches_squashed[0], batches_squashed[1]
 
-
 def build_string_index(string, vocab_size=2 ** 15):
   word_array = np.array(nltk.word_tokenize(string))
 
-  unique, inverse, counts = np.unique(
-    word_array, return_inverse=True, return_counts=True)
+  unique, inverse, counts = np.unique(word_array, return_counts=True)
 
-  max_index = len(unique) - 1
   sort_unique = np.argsort(counts)
-  shuffle_idx = np.searchsorted(counts[sort_unique], counts)
-  unique_sorted = unique[sort_unique][::-1]
+  sorted_counts = counts[sort_unique][::-1][:vocab_size - 1]
+  unique_sorted = unique[sort_unique][::-1][:vocab_size - 1]
 
-  index = np.concatenate((np.array(['UNK']), unique_sorted[:vocab_size - 1]))
-
-  indices = max_index - shuffle_idx + 1
-  indices = np.where(
-    indices > vocab_size - 1,
-    np.zeros(len(indices), dtype=np.int8),
-    indices
-  )
-
-  word_indices = indices[inverse]
-  return index, word_indices
+  return unique_sorted, sorted_counts, word_array
 
 
 def input_from_files(filenames, batch_size, num_epochs=1):
@@ -168,31 +181,49 @@ def input_from_files(filenames, batch_size, num_epochs=1):
       words = tf.parse_single_example(
           serialized_example,
           {
-              'target_words': tf.FixedLenFeature([batch_size], tf.int64),
-              'context_words': tf.FixedLenFeature([batch_size], tf.int64)
+              'target_words': tf.FixedLenFeature([batch_size], tf.string),
+              'context_words': tf.FixedLenFeature([batch_size], tf.string)
           }
       )
       return tf.expand_dims(words['target_words'], 1), words['context_words']
   return _make_input_fn
 
+
 def write_batches_to_file(filename,
                           batch_size,
-                          targets,
-                          contexts,
+                          word_array,
+                          num_skips=8,
+                          skip_window=4,
                           num_shards=4):
-  
-  end = len(targets) - (len(targets) % batch_size)
-  shard_size = end // num_shards
-  for shard in range(0, end, end // num_shards):
-    with tf.python_io.TFRecordWriter('{}-{}.pb2'.format(filename, shard)) as writer:
-      for i in range(shard, shard+shard_size, batch_size):
-        writer.write(
-            tf.train.Example(features=tf.train.Features(feature={
-                'target_words': tf.train.Feature(
-                    int64_list=tf.train.Int64List(
-                        value=targets[i:i+batch_size])),
-                'context_words': tf.train.Feature(
-                    int64_list=tf.train.Int64List(
-                        value=contexts[i:i+batch_size]))
-            })).SerializeToString()
-        )
+    span = 2 * skip_window + 1
+    span_windows = _rolling_window(word_array, span)
+    span_batch_size = batch_size // num_skips
+    span_windows_len = (len(span_windows) // span_batch_size) * span_batch_size
+    span_windows_trunc = span_windows[:span_windows_len]
+    window_batches = np.reshape(
+        span_windows_trunc, (-1, span_batch_size, span))
+
+    shard_size = len(window_batches) // num_shards
+    for shard, index in enumerate(range(0, len(window_batches), shard_size)):
+      shard_file = '{}-{}.pb2'.format(filename, shard)
+      with tf.python_io.TFRecordWriter(shard_file) as writer:
+        for windows in window_batches[index:index+shard_size]:
+          batches = np.apply_along_axis(
+              to_skipgrams, 1, windows, skip_window, num_skips)
+          # Separate targets and contexts
+          batches_sep = np.reshape(batches, (-1, 2, num_skips))
+          # Gather targets and contexts
+          batches_gathered = np.transpose(batches_sep, (1, 0, 2))
+          # Squash targets and contexts
+          batches_squashed = np.reshape(batches_gathered, (2, -1))
+
+          writer.write(
+              tf.train.Example(features=tf.train.Features(feature={
+                  'target_words': tf.train.Feature(
+                      bytes_list=tf.train.BytesList(
+                          value=batches_squashed[0])),
+                  'context_words': tf.train.Feature(
+                      bytes_list=tf.train.BytesList(
+                          value=batches_squashed[1]))
+              })).SerializeToString()
+          )
