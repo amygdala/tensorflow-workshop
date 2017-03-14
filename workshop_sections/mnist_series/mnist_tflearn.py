2f8efcdeded09d84512a45c776c967a669db073e
Yufeng <yufengg@users.noreply.github.com>
2017-03-14 08:25:53 -0700

    Mnist script updates (#60)
    * removed SKCompat and synced py script to match

--- workshop_sections/mnist_series/mnist_tflearn.py
+++ workshop_sections/mnist_series/mnist_tflearn.py
@@ -28,62 +28,80 @@ import time
 import numpy
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-from tensorflow.contrib.learn.python.learn.estimators.estimator import SKCompat
 
 ARGFLAGS = None
 DATA_SETS = None
+BATCH_SIZE = 40
 
 # comment out for less info during the training runs.
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
-def run_linear_classifier():
-    """Run a linear classifier."""
-    global DATA_SETS
-    feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(
-        DATA_SETS.train.images)
-    classifier = SKCompat(tf.contrib.learn.LinearClassifier(
-        feature_columns=feature_columns, n_classes=10))
-    classifier.fit(DATA_SETS.train.images,
-                   DATA_SETS.train.labels.astype(numpy.int64),
-                   batch_size=100, steps=5000)
+# example calls: generate_input_fn(DATA_SETS.train) or generate_input_fn(DATA_SETS.test)
+def generate_input_fn(dataset, batch_size=BATCH_SIZE):
+    def _input_fn():
+        X = tf.constant(dataset.images)
+        Y = tf.constant(dataset.labels.astype(numpy.int64))
+        image_batch, label_batch = tf.train.shuffle_batch([X,Y], 
+                               batch_size=batch_size, 
+                               capacity=8*batch_size, 
+                               min_after_dequeue=4*batch_size,
+                               enqueue_many=True
+                              )
+        return {'pixels': image_batch} , label_batch
 
+    return _input_fn
+
+
+def define_and_run_linear_classifier(num_steps, logdir, batch_size=BATCH_SIZE):
+    """Run a linear classifier."""
+    feature_columns = [tf.contrib.layers.real_valued_column(
+        "pixels", dimension=784)]
+    
+    classifier = tf.contrib.learn.LinearClassifier(
+                feature_columns=feature_columns, 
+                n_classes=10,
+                model_dir=logdir
+                )
+    classifier.fit(
+        input_fn=generate_input_fn(DATA_SETS.train, batch_size=batch_size), 
+        steps=num_steps)
+    
+    print("Finished training.")
+    
     # Evaluate accuracy.
-    accuracy_score = classifier.score(
-        DATA_SETS.test.images,
-        DATA_SETS.test.labels.astype(numpy.int64))['accuracy']
+    accuracy_score = classifier.evaluate(input_fn=generate_input_fn(DATA_SETS.test, batch_size), steps=100)['accuracy']
+    
     print('Linear Classifier Accuracy: {0:f}'.format(accuracy_score))
 
 
-def define_and_run_dnn_classifier():
+def define_and_run_dnn_classifier(num_steps, logdir, lr=.1, batch_size=40):
     """Run a DNN classifier."""
-    global DATA_SETS
-    feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(
-        DATA_SETS.train.images)
-    classifier = SKCompat(tf.contrib.learn.DNNClassifier(
-        feature_columns=feature_columns, n_classes=10,
+    feature_columns = [tf.contrib.layers.real_valued_column("pixels", dimension=784)]
+    
+    classifier = tf.contrib.learn.DNNClassifier(
+        feature_columns=feature_columns, 
+        n_classes=10,
         hidden_units=[128, 32],
-        # After you've done a training run with optimizer learning rate 0.1,
+        optimizer=tf.train.ProximalAdagradOptimizer(learning_rate=lr),
+        model_dir=logdir
+        )
+    # After you've done a training run with optimizer learning rate 0.1,
         # change it to 0.5 and run the training again.  Use TensorBoard to take
-        # a look at the difference.  You can see both runs by pointing it to
-        # the parent model directory, which by default is:
+        # a look at the difference.  You can see both runs by pointing it to the
+        # parent model directory, which by default is:
+        #
         #   tensorboard --logdir=/tmp/tfmodels/mnist_tflearn
-        optimizer=tf.train.ProximalAdagradOptimizer(learning_rate=0.1),
-        model_dir=ARGFLAGS.model_dir
-        ))
-    classifier.fit(DATA_SETS.train.images,
-                   DATA_SETS.train.labels.astype(numpy.int64),
-                   batch_size=100, max_steps=ARGFLAGS.num_steps)
-    print("Finished running the training via the fit() method")
-    return classifier
+        
+    classifier.fit(input_fn=generate_input_fn(DATA_SETS.train, batch_size=batch_size), 
+                   steps=num_steps)
 
+    print("Finished running the deep training via the fit() method")
+    
+    print("\n---Evaluating DNN classifier accuracy...")
+    accuracy_score = classifier.evaluate(input_fn=generate_input_fn(DATA_SETS.test, batch_size=batch_size), 
+                                         steps=100)['accuracy']
 
-def eval_dnn_classifier(classifier):
-    # Evaluate classifier accuracy.
-    global DATA_SETS
-    accuracy_score = classifier.score(
-        DATA_SETS.test.images,
-        DATA_SETS.test.labels.astype(numpy.int64))['accuracy']
     print('DNN Classifier Accuracy: {0:f}'.format(accuracy_score))
 
 
@@ -96,12 +114,11 @@ def main(_):
 
     # Uncomment this if you'd like to run the linear classifier first.
     # print("\n-----Running linear classifier...")
-    # run_linear_classifier()
+    # model_dir = os.path.join("/tmp/tfmodels/mnist_tflearn", str(int(time.time())))
+    # define_and_run_linear_classifier(ARGFLAGS.num_steps, model_dir)
 
     print("\n---- Running DNN classifier...")
-    classifier = define_and_run_dnn_classifier()
-    print("\n---Evaluating DNN classifier accuracy...")
-    eval_dnn_classifier(classifier)
+    define_and_run_dnn_classifier(ARGFLAGS.num_steps, ARGFLAGS.model_dir)
 
 
 if __name__ == '__main__':
